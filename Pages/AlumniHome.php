fcb2ceb4496c12baf8fadf11e422b66e429a3a60
jdibble21 <jdibble@bsu.edu>
2021-05-15 17:42:09 -0500

    Add files and edit home page

--- Pages/AlumniHome.php
+++ Pages/AlumniHome.php
@@ -4,41 +4,13 @@ include 'header.php';
     <!-- Navigation-->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="AlumniHome.php">Start Bootstrap</a>
+            <a class="navbar-brand" href="AlumniHome.php">Alumni Home</a>
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item"><a class="nav-link" href="about.html">About</a></li>
                     <li class="nav-item"><a class="nav-link" href="services.html">Services</a></li>
                     <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" id="navbarDropdownPortfolio" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Portfolio</a>
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
-                            <a class="dropdown-item" href="portfolio-1-col.html">1 Column Portfolio</a>
-                            <a class="dropdown-item" href="portfolio-2-col.html">2 Column Portfolio</a>
-                            <a class="dropdown-item" href="portfolio-3-col.html">3 Column Portfolio</a>
-                            <a class="dropdown-item" href="portfolio-4-col.html">4 Column Portfolio</a>
-                            <a class="dropdown-item" href="portfolio-item.html">Single Portfolio Item</a>
-                        </div>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" id="navbarDropdownBlog" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Blog</a>
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
-                            <a class="dropdown-item" href="blog-home-1.html">Blog Home 1</a>
-                            <a class="dropdown-item" href="blog-home-2.html">Blog Home 2</a>
-                            <a class="dropdown-item" href="blog-post.html">Blog Post</a>
-                        </div>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" id="navbarDropdownPages" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Other Pages</a>
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPages">
-                            <a class="dropdown-item" href="full-width.html">Full Width Page</a>
-                            <a class="dropdown-item" href="sidebar.html">Sidebar Page</a>
-                            <a class="dropdown-item" href="faq.html">FAQ</a>
-                            <a class="dropdown-item" href="404.html">404</a>
-                            <a class="dropdown-item" href="pricing.html">Pricing Table</a>
-                        </div>
-                    </li>
                 </ul>
             </div>
         </div>
@@ -53,7 +25,7 @@ include 'header.php';
             </ol>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img class="d-block w-100" src="https://via.placeholder.com/1900x600" alt="..." />
+                    <img class="d-block w-100" src="../frontend/assets/img/recruitment2017.jpg" alt="..." />
                     <div class="carousel-caption d-none d-md-block">
                         <h3>First Slide</h3>
                         <p>This is a description for the first slide.</p>
@@ -87,28 +59,28 @@ include 'header.php';
     <!-- Page Content-->
     <section class="py-5">
         <div class="container">
-            <h1 class="mb-4">Welcome to Modern Business</h1>
+            <h1 class="mb-4">Welcome Indiana Iota Alumni!</h1>
             <!-- Marketing Icons Section-->
             <div class="row">
                 <div class="col-lg-4 mb-4 mb-lg-0">
                     <div class="card h-100">
-                        <h4 class="card-header">Card Title</h4>
+                        <h4 class="card-header">Catch Up On the Latest News!</h4>
                         <div class="card-body"><p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p></div>
-                        <div class="card-footer"><a class="btn btn-primary" href="#!">Learn More</a></div>
+                        <div class="card-footer"><a class="btn btn-primary" href="#!">View News</a></div>
                     </div>
                 </div>
                 <div class="col-lg-4 mb-4 mb-lg-0">
                     <div class="card h-100">
-                        <h4 class="card-header">Card Title</h4>
-                        <div class="card-body"><p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis ipsam eos, nam perspiciatis natus commodi similique totam consectetur praesentium molestiae atque exercitationem ut consequuntur, sed eveniet, magni nostrum sint fuga.</p></div>
-                        <div class="card-footer"><a class="btn btn-primary" href="#!">Learn More</a></div>
+                        <h4 class="card-header">Donate Directly to the Chapter!</h4>
+                        <div class="card-body"><p class="card-text">Click the link below to donate any amount you wish! Funds go straight to your brothers!</p></div>
+                        <div class="card-footer"><a class="btn btn-primary" href="#!">Donate</a></div>
                     </div>
                 </div>
                 <div class="col-lg-4">
                     <div class="card h-100">
-                        <h4 class="card-header">Card Title</h4>
+                        <h4 class="card-header">Get in touch with the Current Leadership!</h4>
                         <div class="card-body"><p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p></div>
-                        <div class="card-footer"><a class="btn btn-primary" href="#!">Learn More</a></div>
+                        <div class="card-footer"><a class="btn btn-primary" href="#!">Contact Us</a></div>
                     </div>
                 </div>
             </div>
@@ -118,7 +90,7 @@ include 'header.php';
     <!-- Portfolio Section-->
     <section class="py-5 bg-light">
         <div class="container">
-            <h2 class="mb-4">Portfolio Heading</h2>
+            <h2 class="mb-4">Latest Updates</h2>
             <div class="row">
                 <div class="col-lg-4 col-sm-6 mb-4">
                     <div class="card h-100">
@@ -147,64 +119,17 @@ include 'header.php';
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-sm-6 mb-4">
-                    <div class="card h-100">
-                        <a href="#!"><img class="card-img-top" src="https://via.placeholder.com/700x400" alt="..." /></a>
-                        <div class="card-body">
-                            <h4 class="card-title"><a href="#!">Project Four</a></h4>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-sm-6 mb-4">
-                    <div class="card h-100">
-                        <a href="#!"><img class="card-img-top" src="https://via.placeholder.com/700x400" alt="..." /></a>
-                        <div class="card-body">
-                            <h4 class="card-title"><a href="#!">Project Five</a></h4>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-sm-6 mb-4">
-                    <div class="card h-100">
-                        <a href="#!"><img class="card-img-top" src="https://via.placeholder.com/700x400" alt="..." /></a>
-                        <div class="card-body">
-                            <h4 class="card-title"><a href="#!">Project Six</a></h4>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque earum nostrum suscipit ducimus nihil provident, perferendis rem illo, voluptate atque, sit eius in voluptates, nemo repellat fugiat excepturi! Nemo, esse.</p>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
     </section>
     <hr class="my-0" />
-    <!-- Features Section-->
-    <section class="py-5">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-6">
-                    <h2 class="mb-4">Modern Business Features</h2>
-                    <p>The Modern Business template by Start Bootstrap includes:</p>
-                    <ul>
-                        <li><strong>Bootstrap 4.6.0</strong></li>
-                        <li>jQuery 3.5.1</li>
-                        <li>Font Awesome 5.15.3</li>
-                        <li>Working PHP contact form with validation</li>
-                        <li>Unstyled page elements for easy customization</li>
-                    </ul>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, omnis doloremque non cum id reprehenderit, quisquam totam aspernatur tempora minima unde aliquid ea culpa sunt. Reiciendis quia dolorum ducimus unde.</p>
-                </div>
-                <div class="col-lg-6"><img class="img-fluid rounded" src="https://via.placeholder.com/700x450" alt="..." /></div>
-            </div>
-        </div>
-    </section>
     <hr class="my-0" />
     <!-- Call to Action-->
     <aside class="py-5 bg-light">
         <div class="container">
             <div class="row">
-                <div class="col-md-8"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, expedita, saepe, vero rerum deleniti beatae veniam harum neque nemo praesentium cum alias asperiores commodi.</p></div>
-                <div class="col-md-4"><a class="btn btn-lg btn-secondary btn-block" href="#!">Call to Action</a></div>
+                <div class="col-md-8"><p>To keep in touch with the chapter and never miss a thing, follow the National Fraternity on Facebook!</p></div>
+                <div class="col-md-4"><a class="btn btn-lg btn-secondary btn-block" href="https://www.facebook.com/PhiKappaPsi/" target="_blank">Join the Facebook Group!</a></div>
             </div>
         </div>
     </aside>
--- Pages/footer.php
+++ Pages/footer.php
@@ -1,5 +1,5 @@
 <footer class="py-5 bg-dark">
-    <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2021</p></div>
+    <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Phi Kappa Psi 2021</p></div>
 </footer>
 <!-- Bootstrap core JS-->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
