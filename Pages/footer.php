491c8a0da1062c516f1bffd64d56b8e843a9d62a
Jake Dibble <jdibble@bsu.edu>
2021-08-02 10:31:19 -0500

    Edit footer

--- Pages/footer.php
+++ Pages/footer.php
@@ -1,5 +1,5 @@
 <footer id="footerOverride" class="py-5">
-    <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Phi Kappa Psi 2021</p></div>
+    <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Phi Kappa Psi Indiana Iota 2021</p></div>
 </footer>
 <!-- Bootstrap core JS-->
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
